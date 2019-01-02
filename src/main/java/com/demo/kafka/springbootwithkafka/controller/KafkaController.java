2b088e71d37433e9115677bf7fcb08c8b5b55293
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2019-01-02 16:27:36 +0500

    Controller Mapping AddeD

--- src/main/java/com/demo/kafka/springbootwithkafka/controller/KafkaController.java
+++ src/main/java/com/demo/kafka/springbootwithkafka/controller/KafkaController.java
@@ -3,8 +3,12 @@ package com.demo.kafka.springbootwithkafka.controller;
 import com.demo.kafka.springbootwithkafka.service.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "/kafka")
 public class KafkaController {
 
   private final Producer producer;
